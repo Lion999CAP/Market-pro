bac8aa88ae6565aceb5c234e5cb5320f843c65b3
Leandro Magno Lara Santana <96588847+Lion999CAP@users.noreply.github.com>
2024-07-20 20:22:46 -0500

    add mail config

---
+++ backend/src/configs/mail.config.ts
@@ -0,0 +1,18 @@
+/**
+ * Returns a Mailgun client for sending emails.
+ * Adminlocal2k24
+ * @returns {Mailgun.Client} The Mailgun client instance.
+ */
+import FormData from "form-data";
+import Mailgun from "mailgun.js";
+
+export function getClient() {
+    const mailgun = new Mailgun(FormData);
+    const client = mailgun.client({
+        username: "api",
+        key: process.env.MAILGUN_API_KEY || "",
+    });
+
+    return client;
+}
+
